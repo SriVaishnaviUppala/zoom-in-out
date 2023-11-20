ef686e581c5675d56cb2afe09d292d235539a5b8
SriVaishnaviUppala <93321676+SriVaishnaviUppala@users.noreply.github.com>
2023-11-20 14:24:05 +0530

    Add files via upload

---
+++ zoomin&out/app.js
@@ -0,0 +1,13 @@
+var x=document.getElementById("img");
+ function fun(){
+      let width=x.width;
+	  let height=x.height;
+	  x.style.width=(width+100) + "px";
+	  x.style.height=(height+300) + "px";
+}
+function fun1(){
+      let width=x.width;
+	  let height=x.height;
+	  x.style.width=(width-100) + "px";
+	  x.style.height=(height-200) + "px";
+}
